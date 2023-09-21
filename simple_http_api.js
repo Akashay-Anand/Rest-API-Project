28e0275964a7a7c28d5f78193004aa223d2e1721
Akashay Anand <akashayanand1@gmail.com>
2023-09-21 19:24:34 +0530

    Stage-1: CRUD operations on dataset; without framework; complicated

--- simple_http_api.js
+++ simple_http_api.js
@@ -1,12 +1,163 @@
 const http = require('http'); // [Note-01]
 const fs = require('fs'); // [Note-02]
+const data = require('./dataset.json'); //
 
 // [note-03]
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Hello API server!");
-    res.end();
+    const reqURL = req.url;
+    const reqMethod = req.method;
+    switch (reqMethod ) {
+        case 'GET' :
+            getData(req,res);
+            break;
+        case 'POST':
+            postData(req,res);
+            break;
+        case 'PUT':
+            putData(req,res);
+            break;
+        case 'DELETE':
+            deleteData(req,res);
+            break;
+        default: {
+            console.log(typeof( req.url));
+            defaultHandler(res, res);
+        }
+    }
 });
+
+// implement get method on 
+function getData(req, res) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ data }));
+    res.end();
+}
+// implement post method on 
+function postData(req, res) {
+    let body = '';
+    
+    // Collect the incoming JSON data from the request body
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+
+    req.on('end', () => {
+        try {
+            // Parse the incoming JSON data
+            const newData = JSON.parse(body);
+
+            // Add the new data to your existing data array
+            data.push(newData);
+
+            // Write the updated data back to the dataset.json file
+            fs.writeFile('dataset.json', JSON.stringify(data, null, 2), (err) => {
+                if (err) {
+                    console.error('Error writing to dataset.json:', err);
+                    res.writeHead(500, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                } else {
+                    res.writeHead(201, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify({ message: 'Data added successfully' }));
+                }
+            });
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+            res.writeHead(400, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: 'Invalid JSON data' }));
+        }
+    });
+}
+
+// implement put method on
+function putData(req, res) {
+    const reqURL = req.url;
+    // Extract the item ID from the URL or request body (you may need to adjust this)
+    const itemId = reqURL.split('/').pop(); // Extract the last part of the URL as the item ID
+
+    // Find the item in your data array based on the ID
+    const updatedItemIndex = data.findIndex((item) => item.id === parseInt(itemId));
+
+    if (updatedItemIndex === -1) {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: 'Item not found' }));
+    } else {
+        let body = '';
+        
+        // Collect the incoming JSON data from the request body
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        req.on('end', () => {
+            try {
+                // Parse the incoming JSON data
+                const updatedData = JSON.parse(body);
+
+                // Update the item in your data array
+                data[updatedItemIndex] = updatedData;
+
+                // Write the updated data back to the dataset.json file
+                fs.writeFile('dataset.json', JSON.stringify(data, null, 2), (err) => {
+                    if (err) {
+                        console.error('Error writing to dataset.json:', err);
+                        res.writeHead(500, { "Content-Type": "application/json" });
+                        res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                    } else {
+                        res.writeHead(200, { "Content-Type": "application/json" });
+                        res.end(JSON.stringify({ message: 'Data updated successfully' }));
+                    }
+                });
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+                res.writeHead(400, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: 'Invalid JSON data' }));
+            }
+        });
+    }
+}
+
+// implement delete method on
+function deleteData(req, res) {
+    const reqURL = req.url;
+    // Extract the item ID from the URL or request body (you may need to adjust this)
+    const itemId = reqURL.split('/').pop(); // Extract the last part of the URL as the item ID
+
+    // Find the index of the item in your data array based on the ID
+    const deletedItemIndex = data.findIndex((item) => item.id === parseInt(itemId));
+
+    if (deletedItemIndex === -1) {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: 'Item not found' }));
+    } else {
+        // Remove the item from your data array
+        data.splice(deletedItemIndex, 1);
+
+        // Write the updated data back to the dataset.json
+        fs.writeFile('dataset.json', JSON.stringify(data, null, 2), (err) => {
+            if (err) {
+                console.error('Error writing to dataset.json:', err);
+                res.writeHead(500, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            } else {
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ message: 'Data deleted successfully' }));
+            }
+        });
+    }
+}
+
+// this function will execute when the request method is not specified;
+function defaultHandler(req, res) { 
+    res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+    res.write(
+        JSON.stringify({
+          message: `API not found at ${req.url}`,
+        })
+    );
+    res.end();
+}
 
 // [Note-04]
 server.listen(8080,()=>{
@@ -21,7 +172,7 @@ server.listen(8080,()=>{
 - response: this contains response Object for client ; use send data to the client.
 
 > this properties have various method 
-- res.writeHead() ; we are just returning a string in response for client, so have used “Content-type”: “text/plain”.; and the status code is 200( denotes success); 
+- res.writeHead() ; and the status code is 200( denotes success);  we are just returning a string in response for client, so have used “Content-type”: 
 - res.write() ; send text data to the client
 - res.end() ; indicate end of the response
 
