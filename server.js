84d22ed8f92fb1f5ca832ce5c7fb61d428ba5555
Akashay Anand <akashayanand1@gmail.com>
2023-09-24 21:12:36 +0530

    update: add defoult rout ('/') ;

--- server.js
+++ server.js
@@ -20,7 +20,13 @@ app.use(express.json());
 const jobs = require('./routes/jobroutes');
 app.use('/api',jobs); 
 
+// root address
+app.get('/',(req, res) =>{
+    const data = "<h1> Project is Live</h1> <h2>Doc: https://documenter.getpostman.com/view/29937505/2s9YJW5RP7</h2> <h3>[Note:] check doc for routes/endpoints <h3> ";
+    res.status(200).send(data);
 
+    res.end();
+})
 
 
 
