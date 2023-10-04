59d875941949745e1fa342dab3503cfcdb1b1396
Akashay Anand <akashayanand1@gmail.com>
2023-10-05 03:07:48 +0530

    Stage-2.1: Handle uncaught_exception and promises_rejection

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 const errorMiddleware = require('./middlewares/errors');
 require('dotenv').config({path: './config/.env'});
 
+// uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`closing due to uncaught exception`);
+    process.exit(1);
+})
+
 const port = process.env.PORT || 8080;
 console.log(port);
 
@@ -32,7 +39,24 @@ app.get('/',(req, res) =>{
 
 // listen to port
 
-app.listen(port, (err)=>{
-    console.log(`server is listening on ${port}`);
+const live = app.listen(port, (err)=>{
+    console.log(`server is listening on ${port} in ${process.env.NODE_ENV} mode`);
 });
 
+// handle unhandled promises rejection
+process.on('unhandledRejection',err=>{
+    console.log('Error: ' + err.message);
+    console.log(`shutting down the server`);
+    live.close(()=>{
+        process.exit(1);
+    })
+});
+
+console.log(HelloAan)
+
+/*
+In JavaScript, Promises are used for handling asynchronous operations. Promises have two main states: "fulfilled" (resolved) and "rejected" (failed). When a Promise is rejected, it typically means that an error occurred during the execution of an asynchronous task.
+
+An "unhandled promise rejection" is an error that occurs in JavaScript when a Promise object is rejected (i.e., its reject handler is called to indicate failure), but there is no code to handle or catch that rejection. This can lead to unexpected behavior in your application and can make it difficult to debug issues related to asynchronous operations.
+
+*/
